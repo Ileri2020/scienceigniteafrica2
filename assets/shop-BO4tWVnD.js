fb3d45bf4ba6b99728dd80d49c3259c6043c1e8d
ileri2020 <adepojuololade2020@gmail.com>
2024-10-14 17:56:55 +0100

    Updates

---
+++ assets/shop-BO4tWVnD.js
@@ -0,0 +1,1 @@
+import{j as t,m as a}from"./index-CXbzb_MU.js";const o=()=>t.jsx(a.section,{initial:{opacity:0},animate:{opacity:1,transition:{delay:.5,duration:.6,ease:"easeIn"}},className:"container about py-5 justify-center",id:"about",children:"shop"});export{o as default};
--- assets/updates-DZCSHrd9.js
+++
@@ -1,1 +0,0 @@
-import{j as t,m as a}from"./index-C6WG5F-o.js";const i=()=>t.jsx(a.section,{initial:{opacity:0},animate:{opacity:1,transition:{delay:.5,duration:.6,ease:"easeIn"}},className:"container about py-5 justify-center",id:"about",children:"updates"});export{i as default};
